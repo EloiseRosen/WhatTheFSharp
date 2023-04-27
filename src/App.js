01ced65611998a6e5d66a9f7e9a66eab12b78864
EloiseRosen <eloise.rosen@gmail.com>
2023-04-27 05:32:11 -0400

    homepage

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
 
   return (
     <>
-      <div class="main">
+      <div className="main">
 
           <Header />
 
--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <header>
+    <a href="mailto:eloise.rosen+WTFS@gmail.com?subject=Contribute%20an%20Example" id="contribute-example">Contribute an Example</a>
     <h1>What The F#<span className="blink">█</span></h1>
     <h2>A Collection of Surprising Behavior by Programming Language</h2>
     </header>
