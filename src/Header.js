98ae4b7f6fa3797e905eb648b8f74d6fe18a135e
EloiseRosen <eloise.rosen@gmail.com>
2023-04-28 09:47:45 -0400

    update email subject

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <header>
-    <a href="mailto:eloise.rosen+WTFS@gmail.com?subject=Contribute%20an%20Example" id="contribute-example">Contribute an Example</a>
+    <a href="mailto:eloise.rosen+WTFS@gmail.com?subject=What%20the%20F%23%3A%20Contribute%20an%20Example" id="contribute-example">Contribute an Example</a>
     <h1>What The F#<span className="blink">█</span></h1>
     <h2>A Collection of Surprising Behavior by Programming Language</h2>
     </header>
