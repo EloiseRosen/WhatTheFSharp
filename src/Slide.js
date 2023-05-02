89a4cdca179159234d114f13e57108834abd8768
EloiseRosen <eloise.rosen@gmail.com>
2023-05-02 00:32:02 -0400

    refactor into separate SlideMenu component

--- src/Slide.js
+++ src/Slide.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState } from 'react';
+import SlideMenu from  './SlideMenu';
 import { examples } from './examples';
 
 
@@ -6,54 +7,13 @@ function Slide(props) {
   const [slideNum, setSlideNum] = useState(1);
   const examplesForLang = examples[props.selectedLang];
 
-  const handleCopyUrl = () => {
-    navigator.clipboard.writeText(window.location.href).then(
-      () => {console.log('URL successfully copied to clipboard');},
-      (err) => {console.error('error copying URL: ', err);}
-    );
-  };
-
-  const handlePrevClick = useCallback(() => {
-    setSlideNum(Math.max(1, slideNum - 1));
-  }, [slideNum]);
-  const handleNextClick = useCallback(() => {
-    setSlideNum(Math.min(examplesForLang.length, slideNum + 1));
-  }, [slideNum, examplesForLang.length]);
-
-  const handleKeydown = useCallback((event) => {
-    if (event.key === 'ArrowLeft') {
-      handlePrevClick();
-    } else if (event.key === 'ArrowRight') {
-      handleNextClick();
-    }
-  }, [handlePrevClick, handleNextClick]);
-  useEffect(() => {
-    window.addEventListener('keydown', handleKeydown);
-    return () => {window.removeEventListener('keydown', handleKeydown);};
-  }, [handleKeydown]);
-
   return (
   <>
     <div className="slide-box">
       <pre>{examplesForLang[slideNum-1]}</pre>
     </div>
 
-    <div className="nav-slides">
-      <button onClick={handlePrevClick} disabled={slideNum === 1}>
-        <i className="prev-next fa-sharp fa-solid fa-chevron-left"></i>
-      </button>
-
-      <span className="slide-num">{slideNum} / {examplesForLang.length}</span>
-
-      <button onClick={handleCopyUrl}>
-        <i className="copy-url fa-solid fa-link"></i>
-      </button>
-
-      <button onClick={handleNextClick} disabled={slideNum === examplesForLang.length}>
-        <i className="prev-next fa-sharp fa-solid fa-chevron-right"></i>
-      </button>
-    </div>
-
+    <SlideMenu slideNum={slideNum} setSlideNum={setSlideNum} totalSlides={examplesForLang.length}/>
   </>);
 }
 
---
+++ src/SlideMenu.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useCallback } from 'react';
+
+
+function SlideMenu({ slideNum, setSlideNum, totalSlides}) {
+  const handleCopyUrl = () => {
+    navigator.clipboard.writeText(window.location.href).then(
+      () => {console.log('URL successfully copied to clipboard');},
+      (err) => {console.error('error copying URL: ', err);}
+    );
+  };
+
+  const handlePrevClick = useCallback(() => {
+    setSlideNum(Math.max(1, slideNum - 1));
+  }, [slideNum]);
+  const handleNextClick = useCallback(() => {
+    setSlideNum(Math.min(totalSlides, slideNum + 1));
+  }, [slideNum, totalSlides]);
+
+  const handleKeydown = useCallback((event) => {
+    if (event.key === 'ArrowLeft') {
+      handlePrevClick();
+    } else if (event.key === 'ArrowRight') {
+      handleNextClick();
+    }
+  }, [handlePrevClick, handleNextClick]);
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeydown);
+    return () => {window.removeEventListener('keydown', handleKeydown);};
+  }, [handleKeydown]);
+
+  return (
+    <div className="nav-slides">
+      <button onClick={handlePrevClick} disabled={slideNum === 1}>
+        <i className="prev-next fa-sharp fa-solid fa-chevron-left"></i>
+      </button>
+
+      <span className="slide-num">{slideNum} / {totalSlides}</span>
+
+      <button onClick={handleCopyUrl}>
+        <i className="copy-url fa-solid fa-link"></i>
+      </button>
+
+      <button onClick={handleNextClick} disabled={slideNum === totalSlides}>
+        <i className="prev-next fa-sharp fa-solid fa-chevron-right"></i>
+      </button>
+    </div>
+  );
+}
+
+export default SlideMenu;
