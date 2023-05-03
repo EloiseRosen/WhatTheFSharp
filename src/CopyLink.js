77d74e00208980bd25f882bcc5af05d23ecae5a8
EloiseRosen <eloise.rosen@gmail.com>
2023-05-03 13:27:56 -0400

    add indication of successful copy to clipboard

--- src/CopyLink.js
+++ src/CopyLink.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 function CopyLink() {
-  const handleCopyUrl = () => {
+  const [showSuccessTooltip, setShowSuccessTooltip] = useState(false);
+
+  function handleCopyUrl() {
     navigator.clipboard.writeText(window.location.href).then(
-      () => {console.log('URL successfully copied to clipboard');},
-      (err) => {console.error('error copying URL: ', err);}
+      () => {
+        setShowSuccessTooltip(true);
+        setTimeout(() => setShowSuccessTooltip(false), 1000);
+      },
+      (err) => console.error('error copying URL: ', err)
     );
-  };
+  }
 
   return (
-    <button onClick={handleCopyUrl}>
-      <i className="copy-url fa-solid fa-link"></i>
-    </button>
+    <>
+      <button onClick={handleCopyUrl}>
+        <i className="copy-url-icon fa-solid fa-link"></i>
+      </button>
+      {showSuccessTooltip && <span className="copy-success-tooltip">copied</span>}
+    </>
   );
 }
 
