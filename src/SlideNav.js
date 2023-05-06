30697892f18d4bfd08e0f31493125f8efd3aeb24
EloiseRosen <eloise.rosen@gmail.com>
2023-05-06 15:40:44 -0400

    redo slide nav layout

--- src/SlideNav.js
+++ src/SlideNav.js
@@ -18,9 +18,6 @@ function SlideNav({ slideNum, setSlideNum, totalSlides, lang }) {
     navigate(`/${lang}/${newSlideNum}`);
   }, [setSlideNum, navigate, slideNum, totalSlides, lang]);
 
-
-
-
   const handleKeydown = useCallback((event) => {
     if (event.key === 'ArrowLeft') {
       handlePrevClick();
@@ -35,18 +32,21 @@ function SlideNav({ slideNum, setSlideNum, totalSlides, lang }) {
   }, [handleKeydown]);
 
   return (
-    <div className="nav-slides">
-      <button onClick={handlePrevClick} disabled={slideNum === 1}>
-        <i className="prev-next fa-sharp fa-solid fa-chevron-left"></i>
+    <div class="slide-nav-container">
+
+      <button onClick={handlePrevClick} disabled={slideNum === 1} class="prev-button">
+        <i className="prev-next-icon fa-sharp fa-solid fa-chevron-left"></i>
       </button>
 
-      <span className="slide-num">{slideNum} / {totalSlides}</span>
+      <div class="middle-of-navbar">
+        <span>{slideNum}/{totalSlides}</span>
+        <CopyLink />
+      </div>
 
-      <CopyLink />
-
-      <button onClick={handleNextClick} disabled={slideNum === totalSlides}>
-        <i className="prev-next fa-sharp fa-solid fa-chevron-right"></i>
+      <button onClick={handleNextClick} disabled={slideNum === totalSlides} class="next-button">
+        <i className="prev-next-icon fa-sharp fa-solid fa-chevron-right"></i>
       </button>
+
     </div>
   );
 }
