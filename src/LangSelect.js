37379a722650715413f39f6e4203615ab0a3b78a
EloiseRosen <eloise.rosen@gmail.com>
2023-05-06 21:51:39 -0400

    improve dropdown

--- src/LangSelect.js
+++ src/LangSelect.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { examples } from './examples';
 
 
 const allLanguages = Object.keys(examples).sort();
 
 function LangSelect(props) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="select-lang">
-      Select a language ▼
-      <ul>
+      <div className="select-lang-button-content" onClick={() => setIsOpen(!isOpen)}>
+        Select a language ▼
+      </div>
+      {isOpen && (
+      <ul className="select-lang-options">
         {allLanguages.map((language) => (
           <li key={language} onClick={() => props.onLangSelect(language)}>
             {language}
           </li>
         ))}
       </ul>
+      )}
     </div>
   );
 }
